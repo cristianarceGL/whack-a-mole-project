f1fce104cb92b0eee549c507a8fd34e845a06943
Cristian Arce Jimenez <cristian.arce@Cristian-Arce-MacBook-Pro.local>
2021-04-29 09:41:01 -0600

    added solution of the exercise

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
-/****************
- * NOTE:
- *   If you are seeing this message before the PimpMyCode session end, you are cheating, and we do not like cheaters!
- *   well, actually the tv show is cool!, but we are not talking about it.
- *   Be patient, we will be adding the fix in short time, see you later cheater alligator!..
- *
-****************/
+import "./styles.css";
+
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+let lastHole;
+let timeUp = false;
+let score = 0;
+
+//create a function to make a random time for mole to pop from the hole
+function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes) {
+  const index = Math.floor(Math.random() * holes.length);
+  const hole = holes[index];
+
+  //prevent same hole from getting the same number
+  if (hole === lastHole) {
+    return randomHole(holes);
+  }
+  lastHole = hole;
+  return hole;
+}
+
+function peep() {
+  const time = randomTime(500, 1000); //get a random time to determine how long mole should peep
+  const hole = randomHole(holes); //get the random hole from the randomHole function
+  hole.classList.add('up'); //add the CSS class so selected mole can "pop up"
+  setTimeout(() => {
+    hole.classList.remove('up'); //make the selected mole "pop down" after a random time
+    if (!timeUp) {
+      peep();
+    }
+  }, time);
+}
+
+function startGame() {
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  score = 0;
+  peep();
+  setTimeout(() => timeUp = true, 15000) //show random moles for 15 seconds
+}
+
+function wack(e) {
+  if (!e.isTrusted) return; //** new thing I learned */
+  score++;
+  this.parentNode.classList.remove('up'); //this refers to item clicked
+  scoreBoard.textContent = score;
+}
+
+moles.forEach(mole => mole.addEventListener('click', wack))
+
+// Allows the game to start
+window.startGame = startGame;
